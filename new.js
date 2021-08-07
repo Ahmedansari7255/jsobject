d766b45490b5bb26c15d33ef36f186e8025f67d9
Ahmedansari7255 <82463481+Ahmedansari7255@users.noreply.github.com>
2021-08-07 09:42:25 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,63 @@
+
+// var student ={
+//     name:'Ahmed Ansari',
+//     age:21,
+//     address:'jharkhand',
+//     mobile:7255944495,
+// }
+
+// console.log(student.name)
+
+// console.log(student['name']);
+
+
+//2.
+
+
+var student ={
+    name:'Ahmed Ansari',
+    age:21,
+    address:
+    {
+        city:'Dhanbad',
+        state:'jharkhand',
+        pin:828206,
+    },
+    mobile:7255944495,
+}
+
+console.log(student.mobile)
+console.log(student.address.city)
+console.log(student['address']['state'])
+
+//3.
+
+var student ={
+    name:'Ahmed Ansari',
+    age:21,
+    address:
+    {
+        city:'Dhanbad',
+        state:'jharkhand',
+        pin:828206,
+    },
+    mobile:7255944495,
+}
+
+student.graduate=true;
+
+console.log(student.graduate)
+
+//4 method
+
+var video={
+    name:'js class',
+    play: function(){
+        console.log('video played')
+    }
+
+}
+
+console.log(video.name)
+console.log(video.play)
+
